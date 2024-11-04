05af67257cfa34e1f64e5324a97325bcefb1bc10
vvasquez <vvasquez@leapit.com.do>
2024-11-04 09:35:44 -0400

    - creando consulta que busca las especialidades del doctor y crea un objeto de un acceso para que tenga acceso a su especialidad y concatenarla a la consulta de los accesos que ya el ha solicitado

--- design/js/access.js
+++ design/js/access.js
@@ -26,31 +26,43 @@ router.get("/list/faltantes/:id_usuario", async (req, res) => {
 // Obtener el listado de accesos que un usuario ya ha solicitado
 router.get("/list/solicitadas/:id_usuario", async (req, res) => {
     const { id_usuario } = req.params;
+    const accesos_usuario = []
 
     try {
         const query_accesos_solicitados = `
-            SELECT au.id_acc_usuario, au.id_accesos, acc.id_especialidad, MAX(con.fecha_hora) AS fecha_ultima_consulta
+            SELECT au.id_acc_usuario, au.id_accesos, acc.modulo, acc.id_especialidad, MAX(con.fecha_hora) AS fecha_ultima_consulta
             FROM accesos_usuario au
             JOIN accesos acc ON au.id_accesos = acc.id_acessos
             LEFT JOIN consulta con ON con.id_especialidad = acc.id_especialidad
             WHERE au.id_usuario = ${id_usuario}
-            GROUP BY au.id_acc_usuario, au.id_accesos, acc.id_especialidad
+            GROUP BY au.id_acc_usuario, au.id_accesos, acc.id_especialidad, acc.modulo
         `;
-
+        const result_accesos_solicitados = await client.query(query_accesos_solicitados);
+        
         const query_accesos_by_user_especiality = `
-            SELECT au.id_acc_usuario, au.id_accesos, acc.id_especialidad, MAX(con.fecha_hora) AS fecha_ultima_consulta
-            FROM accesos_usuario au
-            JOIN accesos acc ON au.id_accesos = acc.id_acessos
-            LEFT JOIN consulta con ON con.id_especialidad = acc.id_especialidad
-            WHERE au.id_usuario = ${id_usuario}
-            GROUP BY au.id_acc_usuario, au.id_accesos, acc.id_especialidad
+        SELECT a.*, MAX(con.fecha_hora) AS fecha_ultima_consulta
+        FROM accesos a
+        JOIN (
+            SELECT de.id_especialidad 
+            FROM doctor_especialidad de
+            WHERE de.id_doctor = (SELECT id_doctor FROM doctor WHERE id_usuario = ${id_usuario})
+        ) AS esp ON a.id_especialidad = esp.id_especialidad
+        LEFT JOIN consulta con ON con.id_especialidad = a.id_especialidad
+        GROUP BY a.id_acessos
         `;
+        const result_accesos_by_user_especiality = await client.query(query_accesos_by_user_especiality);
+        
+        const accesos_by_user = result_accesos_by_user_especiality.rows.map((acceso)=>{
+            return { id_acc_usuario: null, estado: true, id_accesos: acceso.id_acessos, id_especialidad: acceso.id_especialidad, modulo: acceso.modulo }
+        })
+        
 
-        const accesos_usuario = []
+        console.log('---> ', accesos_by_user);
+        
 
-        const result = await client.query(query_accesos_solicitados);
 
-        accesos_usuario.push(...result.rows)
+
+        accesos_usuario.push(...accesos_by_user, ...result_accesos_solicitados.rows)
 
         res.json(accesos_usuario);
     } catch (error) {
